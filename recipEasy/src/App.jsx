3e4dbcb0e7150825fc34d37df3dee3c2a9a041a9
Jenn Smith <smith.jennifer.rchs@gmail.com>
2022-10-20 22:58:29 -0400

    Made call to the API

---
+++ recipEasy/src/App.jsx
@@ -0,0 +1,15 @@
+import ApiCall from './components/api'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <h1> hello</h1>
+      <ApiCall/>
+    </div>
+  )
+}
+
+export default App
