1cca058aff5ea878da3c3b015a5727763b3481ef
Jenn Smith <smith.jennifer.rchs@gmail.com>
2022-11-05 02:31:23 -0400

    adding first draft of comments feature

--- src/components/FakeRecipe.jsx
+++ src/components/FakeRecipe.jsx
@@ -1,15 +1,31 @@
 // import tan from '../assets/recipeImages/tan.jpeg'
+import { Comment } from "./features/comment"
 export const FakeRecipe = () => {
     let title = document.createElement("h1") 
     title.innerText = "Tandoori Chicken"
     let img = document.createElement("img")
     img.src = 'src/assets/recipeImages/tan.jpeg'
     console.log(img)
+    let directions = document.createElement("div")
+    directions.innerText = 'Lorem ipsum dolor sit amet consectetur adipiscing elitsed do eiusmod.'
+    let commentButton = document.createElement("button")
+    commentButton.innerText = "Comment"
+    let favoriteButton = document.createElement("button")
+    favoriteButton.innerText = "Favorite"
     document.body.appendChild(title)
     document.body.appendChild(img)
+    document.body.appendChild(directions)
+    document.body.appendChild(commentButton)
+    document.body.appendChild(favoriteButton)
+
+  commentButton.addEventListener("click", (e) => {
+    Comment()
+  })
     return (
         <div>
-          
+         
         </div>
     )
 }
+
+
