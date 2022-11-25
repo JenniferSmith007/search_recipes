78500444a49851451ae66a1b7e215c88ef48c35a
Jenn Smith <smith.jennifer.rchs@gmail.com>
2022-11-25 02:12:24 -0500

    testing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 export const Navbar = () => {
 return (
     <div className="nav">
+        <p className="slogan">Recipe - Easy: Your new favorite cookbook</p>
         <Link to='/' className='home'>Home</Link>
-         <Link to='/favorites' className='favorites'>favorite </Link>
+         <Link to='/favorites' className='favorites'>Favorites </Link>
          <Link to='/comments' className='comments'>Comments</Link>
-         <Link to='/random-recipe' className='random-recipe'>Random Recipe</Link>
     </div>
 )
 }
